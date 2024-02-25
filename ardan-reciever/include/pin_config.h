efc78ffdca13bd4f098c33dc3cdb0906e8d131af
dom <dominicgaspeini@gmail.com>
2024-02-25 18:58:20 -0500

    updated for 2.0

--- ardan-reciever/include/pin_config.h
+++ ardan-reciever/include/pin_config.h
@@ -1,16 +1,17 @@
 /**
  * @file pinConfig.h
- * @author dominic gasperini 
+ * @author dominic gasperini
  * @brief this file holds the pin layout for the board I/O
- * @version 1.0
- * @date 2023-04-27
- * 
- * @copyright Copyright (c) 2022
- * 
+ * @version 2.0
+ * @date 2024-02-25
  */
 
+/*
+==========================================================
+                        Includes
+==========================================================
+*/
 
-// includes
 #include <esp_pm.h>
 
 /*
@@ -19,18 +20,16 @@
 ===========================================================
 */
 
-esp_pm_config_esp32_t power_configuration {
-    .max_freq_mhz = 240,   
+esp_pm_config_esp32_t power_configuration{
+    .max_freq_mhz = 240,
     .min_freq_mhz = 240,
     .light_sleep_enable = false,
 };
-
 
 /*
 ===========================================================
                     Pin Definitions
 ===========================================================
 */
-
 
 // Outputs
